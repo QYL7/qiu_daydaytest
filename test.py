37bd4fecccedfb7ca88d9546fd91bbf2236ac175
qiuyaoli <1396875141@qq.com>
2020-03-03 21:33:31 +0800

    wrote a readme file

---
+++ test.py
@@ -0,0 +1,26 @@
+# # lixiaojie={'nose':1,'eyes':2,'face':['big','white']}
+# # print(lixiaojie['face'][0])
+# s={'a':'1','c':'2','b':'3'}
+# print(sorted(s))
+# file = open('test1.txt','w')
+# file.write('hello world!')
+# file.close()
+# f=open('test1.txt')
+# t=f.read()
+# print(t)
+# class work:
+#     def __init__(self, name, pay):
+#         self.name = name
+#         self.pay = pay
+#
+#         def lastName(self):
+#             return self.name.split()[-1]
+#
+#         def giveRaise(self,percent):
+#             return self.pay(1.0+percent)
+
+a=15
+b=3
+print(a/b)
+
+
