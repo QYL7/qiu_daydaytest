bed8ff15044277ed485190e469a46393544a9e3e
qiuyaoli <1396875141@qq.com>
2020-03-09 00:49:35 +0800

    test

--- test.py
+++ test.py
@@ -19,8 +19,33 @@
 #         def giveRaise(self,percent):
 #             return self.pay(1.0+percent)
 
-a=15
-b=3
-print(a/b)
+# age = input("age:")
+# age=int(age)
+# if age < 6:
+#     print("you are kid")
+# elif age > 20:
+#     print("you are adult")
+# else:
+#     print("you are teenager")
 
+# name = ["mike", "lila", 'lulu']
+# for x in name:
+#     print(x)
+# L = ['Bart', 'Lisa', 'Adam']
+# for i in L:
+#     print("hello!"+i)
+
+# while 1:
+#     print('END')
+# dict1 = {"mike": 95, "jack": 98}
+# #print(dict1["mike"])
+# # dict1["jack"]= 88
+# # print("lp" in dict1)
+# # print(dict1.get("mama"))
+# dict1.pop(0)
+# print(dict1)
+# s1=set([1,2,3,3,4])
+# s2=set(['a','b','c'])
+# print(s1&s2)
+# print(s1|s2)
 
